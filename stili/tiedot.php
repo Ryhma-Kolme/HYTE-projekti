02a4d1c2c9c1ab821bc82a2ca5239c0d5148757c
Sini <47240473+sinilahd@users.noreply.github.com>
2020-04-02 18:31:38 +0300

    vähän muutoksii oma tili -sivulle

--- stili/tiedot.php
+++ stili/tiedot.php
@@ -11,21 +11,25 @@ if($_SESSION['sloggedIn']=="yes"){
         <div class="blue-title">
             <h3>Profiili</h3>
         </div>
-        <?php echo("<p> Nimi " .$_SESSION['sfirstName']. "");?>
-        <hr>
-        <?php echo("<p> Maa " .$_SESSION['slastName']. "");?>
-        <hr>
-        <?php echo("<p> Syntymäpäivä " .$_SESSION['spaiva']. "");?>
+        <div class="column-content">
+            <?php echo("<p> Nimi " .$_SESSION['sfirstName']. "");?>
+            <hr>
+            <?php echo("<p> Maa " .$_SESSION['slastName']. "");?>
+            <hr>
+            <?php echo("<p> Syntymäpäivä " .$_SESSION['spaiva']. "");?>
+        </div>
     </div>
     <div class="column">
         <div class="blue-title">
             <h3>Fyysiset tiedot</h3>
         </div>
-        <?php echo("<p> Sukupuoli " .$_SESSION['ssukupuoli']. "");?>
-        <hr>
-        <?php echo("<p> Pituus " .$_SESSION['spituus']. " cm");?>
-        <hr>
-        <?php echo("<p> Paino " .$_SESSION['spaino']. " kg");?>
+        <div class="column-content">
+            <?php echo("<p> Sukupuoli " .$_SESSION['ssukupuoli']. "");?>
+            <hr>
+            <?php echo("<p> Pituus " .$_SESSION['spituus']. " cm");?>
+            <hr>
+            <?php echo("<p> Paino " .$_SESSION['spaino']. " kg");?>
+        </div>
     </div>
 </div>
 </main>
