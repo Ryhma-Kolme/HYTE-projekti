81c10e55d6ecd5297e904b4c7624234bdd1bc2f8
Sini <47240473+sinilahd@users.noreply.github.com>
2020-04-02 15:57:29 +0300

    inavindex -sisältö siirretty alku.php - toimii

--- setusivu/alku.php
+++ setusivu/alku.php
@@ -1,4 +1,39 @@
-<!-- <div class="header-white"> -->
-    <?php include("includes/inavIndex.php");?>
-<!-- </div> -->
+<?php
+//Käyttäjän tila
 
+if($_SESSION['sloggedIn']=="yes"){
+    include("includes/navigaatio.php");
+?>
+<main>
+    <div class="header-white">
+        <?php echo("<p> Tervetuloa " .$_SESSION['suserName']. "!");?>
+        <hr>
+        <!-- <button><a href="logOutUser.php">Kirjaudu ulos</a></button> -->
+    </div>
+    
+    <div class="row">
+        <div class="column">
+            <div class="blue-title">
+                <h3>Tämän päivän ruoat</h3>
+            </div>
+            <p>Taulukko</p>
+            <p>Aamiainen</p>
+            <p>Lounas</p>
+            <p>Välipala</p>
+            <p>Päivällinen</p>
+            <p>Iltapala</p>
+        </div>
+    </div>
+</main>
+<?php
+}else{
+    include("includes/naviOut.php");
+    ?>
+    <main>
+        <div class="header-white">
+            <p class="logo"> foodfx </p>
+            <button><a href="logInUser.php">Kirjaudu</a></button>
+            <button><a href="luoTili2.php">Rekisteröidy</a></button>
+        </div>
+    </main>
+<?php } ?>
