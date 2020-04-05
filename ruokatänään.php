1e448c59a3e4b2668a729a51986d1af0e906bacf
mirakil <49904849+mirakil@users.noreply.github.com>
2020-04-05 17:51:53 +0300

    kommenteja koodiin

--- ruokatänään.php
+++ ruokatänään.php
@@ -1,6 +1,6 @@
 <div class="row-buttons">
         <button><a href="aamiainen.php">Aamiainen</a></button>
-        <button><a href="lounas.php">Lounaus</a></button>
+        <button><a href="lounas.php">Lounas</a></button>
         <button><a href="välipala.php">Välipala</a></button>
         <button><a href="päivällinen.php">Päivällinen</a></button>
         <button><a href="iltapala.php">Iltapala</a></button>
