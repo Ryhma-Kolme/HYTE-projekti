08a0da440cab865bd45b55645ff495008b742603
Sini <47240473+sinilahd@users.noreply.github.com>
2020-04-05 18:13:33 +0300

    Lisäsin noita rinkuloita kuvina

--- skalenteri/kalenteri.php
+++ skalenteri/kalenteri.php
@@ -1,3 +1,3 @@
 <div class="header-white">
-    <h2>Kuukausi</h2>
+    <h2>Maaliskuu</h2>
 </div>
--- skalenteri/yhteenveto.php
+++ skalenteri/yhteenveto.php
@@ -3,13 +3,13 @@
         <div class="blue-title">
             <h3>Keskiarvo päivien ravinnosta</h3>
         </div>
-        <p>rinkula</p>
+        <img style="height:200px; padding:10px" src="images/värit.png" alt="Rinkula">
     </div>
 
     <div class="column">
         <div class="blue-title">
             <h3>Keskiarvo päivien aktiivisuudesta</h3>
         </div>
-        <p>rinkula</p>
+        <img style="height:200px; padding:10px" src="images/värit.png" alt="Rinkula">
     </div>
 </div>
