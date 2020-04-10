ac33605cbbdae92cb25e3576a34daf7d42060a26
Sini <47240473+sinilahd@users.noreply.github.com>
2020-04-10 11:56:37 +0300

    input type hommat takas ja määrä uusiks xd

---
+++ includes/navietusivu.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul> 
+        <div class="left">
+            <li><a class="logo" href="index.php">foodfx</a></li>
+        </div>
+        <div class="right">
+            <li><a href="logOutUser.php"><span class="material-icons">exit_to_app</span></a></li>
+        </div>
+    </ul>
+</nav>
