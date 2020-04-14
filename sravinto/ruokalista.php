0ba1b6033acf4c4f82b42430271fa979b481592c
mirakil <49904849+mirakil@users.noreply.github.com>
2020-04-14 12:36:11 +0300

    hakutoiminto ruokalistaan

---
+++ sravinto/ruokalista.php
@@ -0,0 +1,32 @@
+<?php include("includes/iheader.php");?>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/bootstrap-select.js"></script>
+    <div class="ruoka-tallennus">
+    
+
+                <?php
+                // Ruokien nimien haku kannasta
+                $STH = $DBH->prepare("SELECT foodName FROM app_food ORDER BY foodName ASC");
+                $STH->execute();
+                $tulosOlio=$STH->fetchAll(PDO::FETCH_COLUMN);
+                ?>
+                <form method="post">
+                    <select data-live-search="true" data-live-search-style="startsWith" class="selectpicker" name="food">
+                        <option selected="selected">Lisää ruoka</option>
+                        <?php
+                        
+                        // käydään array läpi
+                        foreach ($tulosOlio as $food){
+                        ?>
+                        <option value="<?php echo strtolower($food); ?>"><?php echo ($food); ?></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                    <input type="text" class="quantitybtn" id="määrä" name="määrä" value="Lisää määrä (g)" onclick="this.select()">
+                    <input type="submit" class="tallennaruoka" name="foodbtn" value="Tallenna ruoka">
+                </form>
+            </div>
--- sravinto/sumFoodsTable.php
+++ sravinto/sumFoodsTable.php
@@ -1,22 +1,22 @@
-<?php
-// Luodaan taulukko johon syötetään arvot 
+<?php // Luodaan taulukko johon syötetään arvot 
+
 echo("<table>
-<tr>
-  <th>Ruoka-aineet yht.</th>
-  <th>Määrä yht.</th>
-  <th>Kalorit yht.</th>
-  <th>Rasvat yht.</th>
-  <th>Hiilihydraatit yht.</th>
-  <th>Proteiinit yht.</th>
-</tr>");
-while	($row=$kysely->fetch()){	
+    <tr>
+      <th>Ruoka-aineet yht.</th>
+      <th>Määrä yht.</th>
+      <th>Kalorit yht.</th>
+      <th>Rasvat yht.</th>
+      <th>Hiilihydraatit yht.</th>
+      <th>Proteiinit yht.</th>
+    </tr>");
+      while	($row=$kysely->fetch()){	
 echo("<tr><td>".$row['COUNT(foodName)']."kpl</td>
-  <td>".$row['SUM(quantity)']."g</td>
-  <td>".$row['SUM(calories)']."g</td>
-  <td>".$row['SUM(fat)']."g</td>
-  <td>".$row['SUM(carbohydrates)']."g</td>
-  <td>".$row['SUM(proteins)']."g</td>");
-                       }
+      <td>".$row['SUM(quantity)']."g</td>
+      <td>".$row['SUM(calories)']."kcal</td>
+      <td>".$row['SUM(fat)']."g</td>
+      <td>".$row['SUM(carbohydrates)']."g</td>
+      <td>".$row['SUM(proteins)']."g</td>");
+                          }
 echo("</table>");
   
 
