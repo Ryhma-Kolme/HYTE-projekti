9b798b7433374c84d82dfa0deafe2400b98f9d2a
Sini <47240473+sinilahd@users.noreply.github.com>
2020-04-15 15:10:59 +0300

    unisivu johonki malliin

--- iUni/unigraafi.php
+++
@@ -1,3 +0,0 @@
-<div style="height: 300px" class="header-white">
-    <img style="height:300px; padding:10px; width: 100%;" src="images/graafi.png" alt="Graafi kuvainnollistamassa">
-</div>
--- iUni/unitiedot.php
+++ iUni/unitiedot.php
@@ -1,9 +1,37 @@
-<div class="row">
+<div class="flex-row">
     <div class="column">
         <div class="blue-title">
-            <h3>Unitiedot</h3>
+            <h3>Viimeisin yö</h3>
         </div>
-        <p>Leposyke</p>
-        <p>Unen kesto</p>
+
+        <div class="uni-column-content">
+            <div class="column-1">
+                <h4>Unen kesto</h4>
+                <img style="width: 100%" src="images/graafi.png" alt="Graafi kuvainnollistamassa">
+            </div>
+
+            <div class="column-2">
+                <h4>Leposyke</h4>
+                <img style="width: 100%" src="images/graafi.png" alt="Graafi kuvainnollistamassa">
+            </div>
+        </div>
+    </div>
+        
+    <div class="column">
+        <div class="blue-title">
+            <h3>Viimeiset 7 päivää</h3>
+        </div>
+
+        <div class="uni-column-content">
+            <div class="column-1">
+                <h4>Unen kesto</h4>
+                <img style="width: 100%" src="images/diagrammi.png" alt="Graafi kuvainnollistamassa">
+            </div>
+            
+            <div class="column-2">
+                <h4>Leposyke</h4>
+                <img style="width: 100%" src="images/graafi.png" alt="Graafi kuvainnollistamassa">
+            </div>
+        </div>
     </div>
 </div>
