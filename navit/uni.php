ed35e8928c9ccf72753e43107309a43becc17cd2
Sini <47240473+sinilahd@users.noreply.github.com>
2020-04-15 18:32:53 +0300

    navin validointivirheet korjattu

--- navit/uni.php
+++ navit/uni.php
@@ -1,14 +1,12 @@
 <nav>
-    <ul> 
-        <div class="left">
-            <li><a class="logo" href="index.php">foodfx</a></li>
-            <li><a href="kuukausi.php">Kalenteri</a></li>
-            <li><a href="ravinto.php">Ravinto</a></li>
-            <li><a class="active" href="unisivu.php">Uni</a></li>
-        </div>
-        <div class="right">
-            <li><a href="tilitiedot.php">Oma tili</a></li>
-            <li><a href="logOutUser.php"><span class="material-icons">exit_to_app</span></a></li>
-        </div>
+    <ul class="left">
+        <li><a class="logo" href="index.php">foodfx</a></li>
+        <li><a href="kuukausi.php">Kalenteri</a></li>
+        <li><a href="ravinto.php">Ravinto</a></li>
+        <li><a class="active" href="unisivu.php">Uni</a></li>
+    </ul>
+    <ul class="right">
+        <li><a href="tilitiedot.php">Oma tili</a></li>
+        <li><a href="logOutUser.php"><span class="material-icons">exit_to_app</span></a></li>
     </ul>
 </nav>
