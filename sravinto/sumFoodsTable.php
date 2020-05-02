35edcb6035a395ad7d484ed8413ab45f44abbdc1
Sini <47240473+sinilahd@users.noreply.github.com>
2020-05-02 14:37:47 +0300

    Ateriataulukot responsiivisix

--- sravinto/sumFoodsTable.php
+++ sravinto/sumFoodsTable.php
@@ -1,8 +1,14 @@
 <!-- Luodaan taulukko johon syötetään arvot  -->
 <table>
-  <tr>
+  <tr class="laptop">
     <th>Ruoka-aineet yht.</th>
     <th>Määrä yht.</th>
+    <th>Kalorit yht.</th>
+    <th>Rasvat yht.</th>
+    <th>Hiilihydraatit yht.</th>
+    <th>Proteiinit yht.</th>
+  </tr>
+  <tr class="mobile">
     <th>Kalorit yht.</th>
     <th>Rasvat yht.</th>
     <th>Hiilihydraatit yht.</th>
@@ -12,9 +18,15 @@
 <?php
 while	($row=$kysely->fetch()){	
   echo("
-    <tr>
+    <tr class='laptop'>
         <td>".$row['COUNT(foodName)']." kpl</td>
         <td>".$row['SUM(quantity)']." g</td>
+        <td>".$row['SUM(calories)']." kcal</td>
+        <td>".$row['SUM(fat)']." g</td>
+        <td>".$row['SUM(carbohydrates)']." g</td>
+        <td>".$row['SUM(proteins)']." g</td>
+    </tr>
+    <tr class='mobile'>
         <td>".$row['SUM(calories)']." kcal</td>
         <td>".$row['SUM(fat)']." g</td>
         <td>".$row['SUM(carbohydrates)']." g</td>
